7c0ddb00c16760b0d9c9023947d93e0e1cf81941
Caroline Belmas <caroline.belmas@gmail.com>
2021-03-02 12:12:46 +0100

    Meal repository created

---
+++ repositories/meal_repository.rb
@@ -0,0 +1,24 @@
+require "csv"
+require_relative "../models/meal"
+
+class MealRepository
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @meals = []
+    @next_id = 1
+    load_csv if File.exist?(@csv_file)
+  end
+
+  private
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id] = row[:id].to_i
+      row[:price] = row[:price].to_i
+      @meals << Meal.new(row)
+    end
+    @next_id = @meals.last.id + 1 unless @meals.empty?
+  end
+
+end
